a288d3453eb1792f07520f67fc593ae184e1095a
Alexis Seurin <alexis.seurin@gmail.com>
2024-07-23 14:20:19 +0200

    Change the YAML deployment file and config files

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/public-transport-map/', // Remplacez par le nom de votre dépôt GitHub
 })
