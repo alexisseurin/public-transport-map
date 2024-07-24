f1afda7a4ec968bd01b1cf865cbb83647a4b2200
Alexis Seurin <alexis.seurin@gmail.com>
2024-07-24 23:40:00 +0200

    Change serve.js

--- serve.js
+++ serve.js
@@ -1,14 +1,33 @@
 import static from 'node-static';
 import http from 'http';
 
-const file = new static.Server('./dist');
+// Création d'un serveur de fichiers statiques avec des types MIME personnalisés
+const fileServer = new static.Server('./dist', {
+  cache: 3600,
+  gzip: true,
+  headers: {
+    'X-Content-Type-Options': 'nosniff',
+  },
+  mime: {
+    'tsx': 'application/typescript',
+    'ts': 'application/typescript',
+  },
+});
 
-http
-  .createServer(function (request, response) {
-    request
-      .addListener('end', function () {
-        file.serve(request, response);
-      })
-      .resume();
-  })
-  .listen(3000);
+const requestHandler = (request, response) => {
+  request.addListener('end', () => {
+    fileServer.serve(request, response, (err, response) => {
+      if (err) {
+        console.error("Error serving " + request.url + " - " + err.message);
+        response.writeHead(err.status, err.headers);
+        response.end();
+      }
+    });
+  }).resume();
+};
+
+const httpServer = http.createServer(requestHandler);
+
+httpServer.listen(3000, () => {
+  console.log('HTTP Server running at http://localhost:3000');
+});
