07830125e1d2d3858139e84c10b3e5e7cadfc01c
Alexis Seurin <alexis.seurin@gmail.com>
2024-07-24 12:36:31 +0200

    Add config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: '/public-transport-map/', // Remplacez par le nom de votre dépôt GitHub
+  build: {
+    outDir: 'dist',  // Le dossier de sortie pour les fichiers transpilés
+  },
 })
