a0aa7c81396e986394f3ae254447ba336d4a5e7a
Alexis Seurin <alexis.seurin@gmail.com>
2024-07-25 10:15:26 +0200

    Change config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/public-transport-map/',
-  build: {
-    outDir: 'dist',  // Le dossier de sortie pour les fichiers transpilés
-  }
-})
+  base: '/public-transport-map/'
+});
